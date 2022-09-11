0663a461f22775251e755b5f0ce5f3838ab3ff96
雨 ʀᴀʏʜᴀɴ サンダー <102699105+RyhnXD@users.noreply.github.com>
2022-09-11 22:48:42 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -12,9 +12,9 @@ let handler = async(m, { conn }) => {
     itemCount : 999999,
     status: 404,
     surface : 404,
-    message: `Nih My Mastah :3`, 
+    message: `Nih My Owner :3`, 
     orderTitle: ``,
-    thumbnail: await (await fetch('https://telegra.ph/file/7501d485189f34dc429a7.jpg')).buffer(), //Gambarnye
+    thumbnail: await (await fetch('https://telegra.ph/file/1665521d176fbf172d735.jpg')).buffer(), //Gambarnye
     sellerJid: '0@s.whatsapp.net' 
     }
     }
@@ -31,9 +31,9 @@ let handler = async(m, { conn }) => {
   return conn.sendButtonDoc(m.chat, `Hay kak @${await m.sender.split('@')[0]}, itu nomor ownerku jangan dispam yah ^_^`, 'Itu Owner Ku Yah Kak Jangan Sungkem Untuk Chat ;3','Credits', '.tqto', m, { quoted: conn.p[id][0],
     contextInfo: { //forwardingScore: 99999, isForwarded: true,
         externalAdReply: { //showAdAttribution: true,
-        	sourceUrl: 'https://vt.tiktok.com/ZSRRmS8vh/',
-            title: 'Jangan Bully Zivfurr Dia Kang Kopas <3',
-            body: 'Follow Tiktok Pacar Saya Bang',
+        	sourceUrl: 'https://wa.me/62895627514070',
+            title: 'Jangan Bully Dia Kang Kopas <3',
+            body: 'Lumimous-md',
           thumbnail: await (await fetch(pp)).buffer()
         }
      }
